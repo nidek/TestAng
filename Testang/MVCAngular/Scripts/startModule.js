4b46249d673b43c81a93ab3eb92e1edc7b23e00f
nidek <martinstark11@gmail.com>
2015-06-16 17:19:14 +0200

    angular routing included

--- Testang/MVCAngular/Scripts/startModule.js
+++ Testang/MVCAngular/Scripts/startModule.js
@@ -1,1 +1,6 @@
-﻿var startModule = angular.module("startModule", []);
+﻿var startModule = angular.module("startModule", ['ngRoute'])
+    .config(function($routeProvider, $locationProvider) {
+    $routeProvider.when('/Start/League', { templateUrl: '/Templates/League.html', controller: 'LeagueController' });
+    $routeProvider.when('/Start/Team', { templateUrl: '/Templates/Team.html', controller: 'TeamController' });
+    $locationProvider.html5Mode(true);
+});
